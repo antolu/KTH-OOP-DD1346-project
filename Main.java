05df35ed11e579240a978a48befb485357a80223
Anton Lu <anton.lu@epfl.ch>
2018-10-29 14:40:16 +0100

    Updated Main for StartFrame

--- Main.java
+++ Main.java
@@ -21,10 +21,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String message = getMessage();
+        new StartFrame();
+        // String message = getMessage();
 
-        Message parsedMessage = Transcriber.parse(message);
+        // Message parsedMessage = Transcriber.parse(message);
 
-        System.out.println(parsedMessage);
+        // System.out.println(parsedMessage);
     }
 }
