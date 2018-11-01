774741bf22550212b5e2465dfea50747a330dacf
Anton Lu <anton.lu@epfl.ch>
2018-11-01 19:12:38 +0100

    Imported wrong package for Node

--- ChatWindow.java
+++ ChatWindow.java
@@ -5,12 +5,12 @@ import javax.swing.text.html.HTMLEditorKit;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.Node;
 
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentType;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import java.io.*;
 import javax.xml.transform.*;
