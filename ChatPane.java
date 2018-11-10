e70ddd2195cbd8f5f88c271f975299142fb11c1a
Anton Lu <anton.lu@epfl.ch>
2018-11-10 15:40:44 +0100

    Should have fixed multipart=start now

--- ChatPane.java
+++ ChatPane.java
@@ -127,7 +127,7 @@ public class ChatPane extends JPanel {
 
         /* Start multipart on all clients also */
         for (User user : users) {
-            user.getClientSocket().send("<message><multipartstart /></message>");
+            user.getClientSocket().send("<message multipart=start></message>");
         }
     }
 
@@ -340,7 +340,7 @@ public class ChatPane extends JPanel {
     }
 
     public void addUser(User user) {
-        user.getClientSocket().send("<message><multipartstart /></message>");
+        user.getClientSocket().send("<message multipart=start></message>");
         addMessage(new Message(user + "connected."));
         users.add(user);
     }
