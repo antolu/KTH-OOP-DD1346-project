811edc8e9c5545245787a987dbf3e10d6b8326a0
Elisabet Arvidsson <elisabetkarvidsson@gmail.com>
2018-11-14 14:29:33 +0100

    initial commit

---
+++ KeyRequestResponse.java
@@ -0,0 +1,22 @@
+/**
+ * Basic class encapsulating a query regarding encryption key request response.
+ */
+public class KeyRequestResponse extends Query {
+    private String key;
+
+    /**
+     * Generic encapsulation constructor.
+     * @param key The key of the encryption type queried.
+     */
+    public KeyRequestResponse(String key) {
+        super("KeyRequestResponse");
+        this.key = key;
+    }
+
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
+}
